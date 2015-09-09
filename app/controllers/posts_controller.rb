0f5d09754fa01dcbad849f29f86a1004bda102d2
bondparkerbond <parkerbond@gmail.com>
2015-09-09 15:57:15 -0600

    Destroy Reviews

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,6 +35,11 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
   private
 
