2621a7bdb63ff6629077a59e87e4ff08e95cb6bb
bondparkerbond <parkerbond@gmail.com>
2015-09-09 15:35:08 -0600

    Add reviews

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,36 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
-  def new
-    @post = Post.new
-  end
 
   def edit
     @post = Post.find(params[:id])
+  end
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :username, :review)
   end
 end
